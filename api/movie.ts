40f0daf012ab98fd90adf2e894f70e387d75fe65
blzniti <beelazy1579@gmail.com>
2024-03-09 21:56:38 +0700

    ิAdv.web

--- api/movie.ts
+++ api/movie.ts
@@ -31,7 +31,13 @@ router.post("/", (req, res) => {
 
   let sql =
     "INSERT INTO `Movie` (`Name`, `Poster`, `Runtime`, `Plot`, `Year`) VALUES (?, ?, ?, ?, ?)";
-  sql = mysql.format(sql, [movie.Name, movie.Poster, movie.Runtime, movie.Plot, movie.Year]);
+  sql = mysql.format(sql, [
+    movie.Name,
+    movie.Poster,
+    movie.Runtime,
+    movie.Plot,
+    movie.Year,
+  ]);
 
   conn.query(sql, (err, result) => {
     if (err) {
@@ -47,18 +53,15 @@ router.post("/", (req, res) => {
   });
 });
 
-
 router.delete("/:id", (req, res) => {
   let id = +req.params.id;
   conn.query("delete from Movie where Mid = ?", [id], (err, result) => {
-     if (err) throw err;
-     res
-       .status(200)
-       .json({ affected_row: result.affectedRows });
+    if (err) throw err;
+    res.status(200).json({ affected_row: result.affectedRows });
   });
 });
 
-// get by name ID 
+// get by name ID
 // router.get("/search/fields", (req, res) => {
 //   conn.query(
 //     "select * from Movie where (Mid IS NULL OR Mid = ?) OR (Name IS NULL OR Name like ?)",
@@ -70,28 +73,56 @@ router.delete("/:id", (req, res) => {
 //   );
 // });
 
+// router.get("/search/fields", (req, res) => {
+//   const { name } = req.query;
+
+//   conn.query(`
+//     SELECT
+//         Movie.*,
+//         Stars.*,
+//         Person.Type AS TypeS,
+//         Creators.*,
+//         PersonCreator.Type AS TypeC
+//     FROM
+//         Movie
+//     LEFT JOIN
+//         Stars ON Movie.Mid = Stars.Mid
+//     LEFT JOIN
+//         Creators ON Movie.Mid = Creators.Mid
+//     LEFT JOIN
+//         Person ON Stars.Pid_S = Person.Pid
+//     LEFT JOIN
+//         Person AS PersonCreator ON Creators.Pid_C = PersonCreator.Pid
+//     WHERE
+//         Movie.Name LIKE ?
+//   `, [`%${name}%`], (err, result, fields) => {
+//     if (err) {
+//       console.error("Error executing SQL query:", err);
+//       res.status(500).json({ error: "An error occurred while executing the SQL query." });
+//       return;
+//     }
+//     res.json(result);
+//   });
+// });
+
 router.get("/search/fields", (req, res) => {
   const { name } = req.query;
 
   conn.query(`
     SELECT 
         Movie.*, 
-        Stars.*,
-        Person.Type AS TypeS,
-        Creators.*,
-        PersonCreator.Type AS TypeC
+        GROUP_CONCAT(DISTINCT CONCAT(Stars.Name_S, ' (', Stars.Age_S, ' years, ', Stars.Image_S, ', ', Stars.Detail_S, ')')) AS Stars,
+        GROUP_CONCAT(DISTINCT CONCAT(Creators.Name_C, ' (', Creators.Age_C, ' years, ', Creators.Image_C, ', ', Creators.Detail_C, ')')) AS Creators
     FROM 
         Movie
     LEFT JOIN 
         Stars ON Movie.Mid = Stars.Mid
     LEFT JOIN 
         Creators ON Movie.Mid = Creators.Mid
-    LEFT JOIN
-        Person ON Stars.Pid_S = Person.Pid
-    LEFT JOIN
-        Person AS PersonCreator ON Creators.Pid_C = PersonCreator.Pid
     WHERE 
         Movie.Name LIKE ?
+    GROUP BY
+        Movie.Mid
   `, [`%${name}%`], (err, result, fields) => {
     if (err) {
       console.error("Error executing SQL query:", err);
